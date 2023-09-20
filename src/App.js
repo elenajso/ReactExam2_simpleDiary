40b94de147af66772fdb2dd529f4363572dfbfba
mikyung.jung <mikyung.jung@hyosung.com>
2023-09-20 09:48:56 +0900

    설명: 섹션5 실습

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>일기장
+        <DiaryEditor/>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
